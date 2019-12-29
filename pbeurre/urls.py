6109c741762f84982c277caccebdd94a5f4dcca5
unknown <mfjotaa@hotmail.com>
2019-12-29 14:09:45 +0100

    correct

--- pbeurre/urls.py
+++ pbeurre/urls.py
@@ -21,21 +21,21 @@ urlpatterns = [
 ]"""
 
 from django.conf import settings
-from django.urls import include, path
+from django.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from aliments import views
 
 urlpatterns = [
-    path('', views.index,  name='index'),
-    path('aliments/', include('aliments.urls')),
-    path('content/', admin.site.urls),
+    url(r'^$', views.index,),
+    url(r'^aliments/', include('aliments.urls')),
+    url(r'^content/', admin.site.urls),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
+        url('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns + static(
         settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
