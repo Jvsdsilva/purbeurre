12087c863997583cc4287eb0146376aef1796a82
Jvsdsilva <mfjotaa@hotmail.com>
2020-01-06 19:11:32 +0100

    deploie herok

--- pbeurre/settings.py
+++ pbeurre/settings.py
@@ -131,7 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = "bootstrap4"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 TEMPLATE_DEBUG = DEBUG
 
@@ -147,7 +146,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     # Static files settings
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
